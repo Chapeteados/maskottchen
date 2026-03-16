c4da9448aa0c5944e746b07f3dc518bee75e74d0
Emmanuel Valenzuela <manu@MacBook-Air-de-Regina.local>
2026-03-16 15:27:03 -0600

    core: init project and navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  fonts: [{
+    provider: fontProviders.google(),
+    name: 'Maven Pro',
+    cssVariable: '--font-maven-pro',
+    weights: [400, 500, 600, 700, 800, 900]
+  },
+  {
+    provider: fontProviders.googleicons(),
+    name: 'Material Symbols Outlined',
+    cssVariable: '--font-icons',
+    options: {
+      experimental: {
+        glyphs: ["menu", "close"],
+      },
+    }
+  }],
+});
