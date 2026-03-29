3c6fa407abd2e9954096b1eb890b67fd2e4c2e7b
Emmanuel Valenzuela <manuvaglz@gmail.com>
2026-03-28 20:43:02 -0600

    feat: temp map

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,5 +3,5 @@ export const STRAPI_BASE_URL_DEV = "http://localhost:1337";
 export const STRAPI_BASE_URL_PROD = "https://maskottchen-be-production.up.railway.app";
 
 /** Enlaces a redes (https absolutas). Dejar `""` para ocultar o usar `#` desde el componente. */
-export const FACEBOOK_URL = "";
+export const FACEBOOK_URL = "https://www.facebook.com/Maskottchen.petfood";
 export const INSTAGRAM_URL = "";
