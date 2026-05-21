522c32233298902bdca1e422cbb17da467079a50
Emmanuel Valenzuela <manuvaglz@gmail.com>
2026-05-20 18:34:46 -0600

    update constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 /** URL base de Strapi (sin barra final). */
 export const STRAPI_BASE_URL_DEV = "http://localhost:1337";
-export const STRAPI_BASE_URL_PROD = "https://maskottchen-be-production.up.railway.app";
+export const STRAPI_BASE_URL_PROD = "https://maskottchen-b-production.up.railway.app";
 
 /** Enlaces a redes (https absolutas). Dejar `""` para ocultar o usar `#` desde el componente. */
 export const FACEBOOK_URL = "https://www.facebook.com/Maskottchen.petfood";
